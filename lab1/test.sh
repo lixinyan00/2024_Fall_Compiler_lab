28cf6c31331a9d4ba9d58603795141fce26678ac
lixinyan00 <lixinyan00@126.com>
2024-11-01 11:55:08 +0800

    finish lab1

--- lab1/test.sh
+++ lab1/test.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 flex lexical_analysis.l
-bison -d syntax.y
+bison -d -t -v syntax.y
 gcc main.c syntax.tab.c -lfl -ly -o parser
+cd test1
+python3 test.py -p ../parser -g 2
+cd ..
--- lab1/test2024.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-for file in ./Tests_2024/inputs/{A-,B-,D-}*.cmm; do 
-    echo "$file"
-    ./parser "$file"; 
-    
-done
